9c802c2798dd77c546d63b5e91145adb66f3a8ce
Abner <abnerisraelrosales@hotmail.com>
2023-08-28 22:05:01 -0600

    A login form was added for users to log in, and upon entering the correct data, it redirects you to a newly created page named main-page.

---
+++ scripts/main.js
@@ -0,0 +1,16 @@
+// main.js
+document.getElementById("loginForm").addEventListener("submit", function(event) {
+    event.preventDefault(); // Evita que el formulario se envíe automáticamente
+
+    // Obtiene los valores ingresados por el usuario
+    var email = document.getElementById("exampleInputEmail1").value;
+    var password = document.getElementById("inputPassword6").value;
+
+    // Verifica si las credenciales son correctas (ajusta las validaciones según tus necesidades)
+    if (email === "abnerhdz@hotmail.com" && password === "12345abc") {
+        // Abre una nueva ventana //
+        window.open("main-page.html", "_blank");
+    } else {
+        alert("Credenciales incorrectas. Por favor, intenta de nuevo.");
+    }
+});
